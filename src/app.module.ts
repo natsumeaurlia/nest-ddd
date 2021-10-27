023f2913c09d2d70b70f3b8f770c1135ecfedaee
natsume0718 <moto718program@gmail.com>
2021-10-28 03:09:51 +0900

    change dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app/controller/app.controller';
+import { AppService } from './domain/service/app.service';
 
 @Module({
   imports: [],

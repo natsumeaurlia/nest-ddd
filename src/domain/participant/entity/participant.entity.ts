b127b70f3aa91592d6b9fc70ebadc69630f333b3
natsume0718 <moto718program@gmail.com>
2022-01-09 18:07:16 +0900

    constructor追加

--- src/domain/participant/entity/participant.entity.ts
+++ src/domain/participant/entity/participant.entity.ts
@@ -2,7 +2,14 @@ import { IsNotEmpty, IsString } from 'class-validator';
 import { status } from '../vo/participant-stauts.vo';
 
 export class Participant {
-  readonly _id?: number;
+  readonly _id: number;
+
+  constructor({ id, name, email, status }) {
+    this._id = id;
+    this.name = name;
+    this.email = email;
+    this.status = status;
+  }
 
   @IsString()
   @IsNotEmpty()

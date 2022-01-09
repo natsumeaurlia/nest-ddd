96139f1b8468208b24b3157fd1b8def3b7a97fc9
natsume0718 <moto718program@gmail.com>
2022-01-09 18:08:55 +0900

    リポジトリ追加

---
+++ src/infrastructure/db/repository/participant.repository.ts
@@ -0,0 +1,19 @@
+import { ParticipantRepositoryInterface } from '@domain/participant/participant.repository.interface';
+import { Injectable } from '@nestjs/common';
+import { Participant } from '@domain/participant/entity/participant.entity';
+import { PrismaService } from '@util/prisma.servie';
+
+@Injectable()
+export class ParticipantRepository implements ParticipantRepositoryInterface {
+  constructor(private prisma: PrismaService) {}
+
+  public async find(participantId: number): Promise<Participant | null> {
+    const result = await this.prisma.participant.findUnique({
+      where: { id: participantId },
+    });
+    if (result) {
+      return new Participant(result);
+    }
+    return null;
+  }
+}

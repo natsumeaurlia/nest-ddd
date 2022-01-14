f23611abb2eb9c9ae23dbdbb6c877e6cf680eb4c
natsume0718 <moto718program@gmail.com>
2022-01-14 17:38:07 +0900

    名前変更

--- src/util/prisma.service.ts
+++ src/util/prisma.service.ts
@@ -1,6 +1,9 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+/**
+ * @see https://www.prisma.io/docs/guides/performance-and-optimization/connection-management#re-using-a-single-prismaclient-instance
+ */
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {

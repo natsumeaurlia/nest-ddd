f216b5faec2d18d1349228817d279de9cf1aa546
natsume0718 <moto718program@gmail.com>
2022-01-15 00:07:29 +0900

    domain service追加

---
+++ src/domain/participant/service/participant.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { ParticipantRepositoryInterface } from '@domain/participant/participant.repository.interface';
+import { CreateParticipantDto } from '@app/dto/create-participant.dto';
+import { ParticipantStatus } from '@domain/participant/vo/participant-stauts.vo';
+
+@Injectable()
+export class ParticipantService {
+  constructor(private repo: ParticipantRepositoryInterface) {}
+
+  async create(props: CreateParticipantDto) {
+    if (await this.repo.findByEmail(props.email)) {
+      throw new Error('Duplicate Email');
+    }
+    const status = ParticipantStatus.create();
+    const result = await this.repo.create(
+      props.name,
+      props.email,
+      status.getStatus(),
+    );
+    return Boolean(result);
+  }
+}

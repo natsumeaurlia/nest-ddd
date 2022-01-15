99e0d14629fb8cd79c85a2e3d91af4b3a0194053
natsume0718 <moto718program@gmail.com>
2022-01-15 23:42:30 +0900

    - 各種moduleをdiできるようにする - log middleware追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
-import { Module } from '@nestjs/common';
+import {
+  Global,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ParticipantController } from './controller/participant.controller';
+import { ParticipantUsecase } from '@app/usecase/participant.usecase';
+import { ParticipantService } from '@domain/participant/service/participant.service';
+import { ParticipantRepository } from '@infrastructure/db/repository/participant.repository';
+import { PrismaService } from '@util/prisma.service';
+import { LoggerMiddleware } from '@app/middleware/logger.middleware';
 
+@Global()
 @Module({
   imports: [],
   controllers: [ParticipantController],
-  providers: [],
+  providers: [
+    PrismaService,
+    ParticipantService,
+    ParticipantUsecase,
+    ParticipantRepository,
+    {
+      provide: 'ParticipantRepositoryInterface',
+      useClass: ParticipantRepository,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}

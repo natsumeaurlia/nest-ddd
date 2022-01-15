7b602973845fb06261af7b2503313939ad8da9dc
natsume0718 <moto718program@gmail.com>
2022-01-15 23:54:10 +0900

    一旦interfaceをcreateのみにする

--- src/app/usecase/participant.usecase.ts
+++ src/app/usecase/participant.usecase.ts
@@ -1,26 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { CreateParticipantDto } from './dto/create-participant.dto';
-import { UpdateParticipantDto } from './dto/update-participant.dto';
+import { CreateParticipantDto } from '@app/dto/create-participant.dto';
+import { ParticipantService } from '@domain/participant/service/participant.service';
 
 @Injectable()
 export class ParticipantUsecase {
+  constructor(private readonly participantService: ParticipantService) {}
+
   create(createParticipantDto: CreateParticipantDto) {
-    return 'This action adds a new participant';
-  }
-
-  findAll() {
-    return CreateParticipantDto;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} participant`;
-  }
-
-  update(id: number, updateParticipantDto: UpdateParticipantDto) {
-    return `This action updates a #${id} participant`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} participant`;
+    return this.participantService.create(createParticipantDto);
   }
 }
